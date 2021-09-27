342e10c83ed314b9988ad59b25b856ef9cbc5bad
abedshadin <60042717+abedshadin@users.noreply.github.com>
2021-09-28 01:12:35 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,7 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("Facebook", "https://fb.com/abedshadin")    
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
